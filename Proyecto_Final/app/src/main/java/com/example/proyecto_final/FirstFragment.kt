6490c0c95f8dbc0c21e93c2b51fb8fa15d3cc9e7
LAPTOP-I3P3593I\Guillermo <guillermo.jabato.p@gmail.com>
2022-05-23 16:43:50 +0200

    diseño secondFragment

--- Proyecto_Final/app/src/main/java/com/example/proyecto_final/FirstFragment.kt
+++ Proyecto_Final/app/src/main/java/com/example/proyecto_final/FirstFragment.kt
@@ -1,12 +1,10 @@
 package com.example.proyecto_final
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.proyecto_final.databinding.FragmentFirstBinding
 import com.google.firebase.firestore.FirebaseFirestore
