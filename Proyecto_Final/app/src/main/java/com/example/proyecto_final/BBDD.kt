7bdea4e635e70996aa2e4280cf1954bf954a8b08
LAPTOP-I3P3593I\Guillermo <guillermo.jabato.p@gmail.com>
2022-05-23 17:04:08 +0200

    Controlador de BBDD

--- Proyecto_Final/app/src/main/java/com/example/proyecto_final/BBDD.kt
+++ Proyecto_Final/app/src/main/java/com/example/proyecto_final/BBDD.kt
@@ -1,5 +1,32 @@
 package com.example.proyecto_final
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 class BBDD {
 
+    private val db = FirebaseFirestore.getInstance()
+
+    fun guardar_usuario(email: String, pass: String): Boolean {
+        var bol = false
+
+        db.collection("users").document(email).get()
+            .addOnSuccessListener {
+                if (email == it.get("email") && pass == it.get("password")
+                ) {
+                    bol = true
+                } else {
+                    db.collection("users")
+                        .document(email)
+                        .set(
+                            hashMapOf(
+                                "email" to email,
+                                "password" to pass
+                            )
+                        )
+                    bol = false
+                }
+
+            }
+        return bol
+    }
 }
